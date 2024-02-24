4067630a262ee43607a326be66e331493f1caa93
Spacely-12 <109187260+Spacely-12@users.noreply.github.com>
2024-02-24 16:20:50 +0000

    addition of css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="resources\css\app.css">
     <title>Attendance Monitoring System</title>
 </head>
 <body>
